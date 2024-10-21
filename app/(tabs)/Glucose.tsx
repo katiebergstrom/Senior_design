f76ea76b145ae515a9ecb77d2c4ba867d9b600f8
arhorning17 <arhorning17@gmail.com>
2024-10-21 19:25:47 -0400

    Add files via upload

---
+++ app/(tabs)/Glucose.tsx
@@ -0,0 +1,90 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, View, Dimensions, Image, Platform } from 'react-native';
+import React from 'react';
+import { LineChart } from "react-native-chart-kit";
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Graph</ThemedText>
+      </ThemedView>
+      <View>
+      <ThemedText>
+          Sample Line Chart
+        </ThemedText>
+        <LineChart
+          data={{
+            labels: ["6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={240}
+          yAxisSuffix="mg"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: 'white',
+            backgroundGradientFrom: 'green',
+            backgroundGradientTo: 'green',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 20
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 10,
+            borderRadius: 16
+          }}
+        />
+      </View>
+    </ParallaxScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
